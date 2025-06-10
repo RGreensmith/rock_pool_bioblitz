e0d1820da6997610ca48346bc77a24bdcd8f92c8
RGreensmith <rose.greensmith@yahoo.co.uk>
2025-06-10 14:30:35 +0100

    got number of records within 2km of each non native record coord in playgroundSpatialBinning from NBN Atlas API

--- Playground/playgroundSpatialBinning.R
+++ Playground/playgroundSpatialBinning.R
@@ -81,12 +81,13 @@ head(obs_points)
 # Download may lead to professional publication (including universities,
 # NGOs etc. where people are being paid to research and publish)",
 # "id":17,"deprecated":false}
+api = "https://records-ws.nbnatlas.org/occurrences/search?q=lsid%3ANHMSYS0021060242&fq=occurrence_status%3Apresent&fq=-(identification_verification_status%3A%22Unconfirmed%22%20OR%20identification_verification_status%3A%22Unconfirmed%20-%20not%20reviewed%22%20OR%20identification_verification_status%3A%22Unconfirmed%20-%20plausible%22)"
 
 api = "https://records-ws.nbnatlas.org/index/fields"
 taxonInfo = GET(api)
 taxonInfoContent = httr::content(taxonInfo, as = 'text')
 taxonInfoContentJSON = jsonlite::fromJSON(taxonInfoContent)
-
+df=taxonInfoContentJSON$occurrences
 ###########################################################################
 # records of Sargassum muticum within 100km (radius=100.0) of a point in London
 # api2 = "https://records-ws.nbnatlas.org/occurrences/index/download?reasonTypeId=10&q=*:*&fq=genus:Sargassum&lat=51.5074&lon=0.1278&radius=100.0&qa=none"
@@ -100,63 +101,54 @@ df = readr::read_csv(unz(description = "C:/Users/Rose/rock_pool_bioblitz/data.zi
 df=as.data.frame(df)
 numRecords = length(df[,1])
 ################################################################################
-# Creating data frame for number of inat records at least 1km away from NBN Atlas records
-nnSppCNm=unique(as.character(natbioblitz_nns$taxon.common_name.name))
-nnSppSNm=unique(as.character(natbioblitz_nns$taxon.name))
-recordsInBuffer = matrix(ncol = 4, nrow = length(nnSppCNm))
-recordsInBuffer=as.data.frame(recordsInBuffer)
-row.names(recordsInBuffer)=nnSppSNm
-names(recordsInBuffer)=c("latin_name","NBN_recs_in_inat_buffer",
-                         "total_inat_recs",
-                         "percent_new_recs")
-recordsInBuffer$latin_name = latinName
-
-NBN_recs = rep(NA, times = length(natbioblitz_nns[,1]))
+# Number of inat records at least 1km away from NBN Atlas records
 
 # Combine the inat_data dataframe with the new columns
+NBN_recs = rep(NA, times = length(natbioblitz_nns[,1]))
 natbioblitz_nns = cbind(natbioblitz_nns,NBN_recs)
 
-for (a in 1:length(nnSppSNm)) {
-  natbioblitz_nns_filtered = filter(natbioblitz_nns,
-                                    taxon.name == nnSppSNm[a])
-  binomClassNm = natbioblitz_nns_filtered$taxon.name[1]
+for (a in 22:length(natbioblitz_nns[,1])) {
+  binomClassNm = natbioblitz_nns$taxon.name[a]
   binomClassNmSplit = strsplit(binomClassNm,"[ ]")
   genus = binomClassNmSplit[[1]][1]
   species = binomClassNmSplit[[1]][2]
   
-  for (b in 1:length(natbioblitz_nns_filtered[,1])) {
-    ####################### Get NBN Atlas Records for each point #################
-    # records of Sargassum muticum within 100km (radius=100.0) of a point in London
-    
-    # Paste the genus and species names into the NBN Atlas API key and download relevant data
-    lat = natbioblitz_nns_filtered$latitude[b]
-    lon = natbioblitz_nns_filtered$longitude[b]
-    
-    api = paste("https://records-ws.nbnatlas.org/occurrences/index/download?reasonTypeId=17&q=*:*&fq=genus:",
-                genus,
-                "&facets=rk_species:",
-                species,
-                "&lat=",
-                lat,
-                "&lon=",
-                lon,
-                "&radius=10.0&qa=none",
-                "&flimit=10",
-                sep = "")
-    taxonInfo = GET(api)
-    
-    download.file(url = api, 
-                  destfile = "C:/Users/Rose/rock_pool_bioblitz/data.zip", 
-                  mode = "wb")
-    dfNBN = readr::read_csv(unz(
-      description = "C:/Users/Rose/rock_pool_bioblitz/data.zip",
-      filename = "data.csv"))
-    dfNBN=as.data.frame(dfNBN)
-    numRecords = length(dfNBN[,1])
-    
-    recordsInBuffer$NBN_recs_in_inat_buffer[a]=recordsInBuffer$NBN_recs_in_inat_buffer[a]+numRecords
-    natbioblitz_nns_filtered$NBN_recs[b]=numRecords
-  }
+  # Paste the genus and species names into the NBN Atlas API key and download relevant data
+  lat = natbioblitz_nns$latitude[a]
+  lon = natbioblitz_nns$longitude[a]
+  
+  api = paste("https://records-ws.nbnatlas.org/occurrences/index/download?reasonTypeId=17&q=*:*&fq=genus:",
+              genus,
+              "&facets=rk_species:",
+              species,
+              "&lat=",
+              lat,
+              "&lon=",
+              lon,
+              "&radius=2.0&qa=none",
+              sep = "")
+  print(paste("a =",a))
+  print(Sys.time())
+  # pause for 1 minute
+  Sys.sleep(60)
+  print(Sys.time())
+  
+  download.file(url = api, 
+                destfile = "C:/Users/Rose/rock_pool_bioblitz/data.zip", 
+                mode = "wb")
+  dfNBN = readr::read_csv(unz(
+    description = "C:/Users/Rose/rock_pool_bioblitz/data.zip",
+    filename = "data.csv"))
+  dfNBN=as.data.frame(dfNBN)
+  
+  numRecords = length(dfNBN[,1])
+  natbioblitz_nns$NBN_recs.1[a]=numRecords
+  print(paste(natbioblitz_nns$taxon.common_name.name[a],"record =",natbioblitz_nns$NBN_recs.1[a]))
+  rm(dfNBN)
 }
+
+
+
+#########
 #############
 #End
